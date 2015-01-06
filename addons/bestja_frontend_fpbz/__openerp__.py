25776e45f6ba49bdd231731ac2c0c0f5401c8ffb
KamilWo <kamil.m.wozniak+bitbucket@gmail.com>
2015-01-06 12:47:59 +0100

    Skórka FPBŻ: blok dużego zdjęcia, menu górnego, logo oraz stopka

---
+++ addons/bestja_frontend_fpbz/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    # Theme information
+    'name': "FPBŻ frontend",
+    'description': """
+    This is a custom theme made for Federacja Polskich Banków Żywności
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'css': ['static/src/css/custom.css'],
+    'depends': ['website'],
+
+    # templates, pages, and snippets
+    'data': [
+        'views/config.xml',
+        'views/pages.xml',
+        'views/snippets.xml',
+    ],
+    'application': True,
+    # Your information
+    'author': "Laboratorium EE, Kamil Woźniak",
+    'website': "http://laboratorium.ee",
+}
