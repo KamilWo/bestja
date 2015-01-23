56feb5cacc0789a09c2c4cb0d1808739a13da8d1
KamilWo <kamil.m.wozniak+bitbucket@gmail.com>
2015-01-23 10:56:09 +0100

    Main page which contains: eight blocks according to the new look of GUI + footer
    Conflicts:
    	addons/bestja_account_deletion/models.py
    	addons/bestja_configuration_ucw/__openerp__.py
    	addons/bestja_frontend_fpbz/__openerp__.py
    	addons/bestja_frontend_fpbz/static/src/css/custom.css
    	addons/bestja_frontend_fpbz/static/src/img/fb.png
    	addons/bestja_frontend_fpbz/static/src/img/myszka.png
    	addons/bestja_frontend_fpbz/static/src/img/stopka_ikonki.png
    	addons/bestja_frontend_fpbz/static/src/js/custom.js
    	addons/bestja_frontend_fpbz/templates/pages/footer.xml
    	addons/bestja_frontend_fpbz/templates/pages/home_page.xml
    	addons/bestja_frontend_fpbz/templates/snippets/main_top_white_text.xml
    	addons/bestja_frontend_fpbz/templates/snippets/map_of_poland.xml
    	addons/bestja_frontend_fpbz/templates/snippets/meet_our_volunteers.xml
    	addons/bestja_frontend_fpbz/templates/snippets/quote_div.xml
    	addons/bestja_frontend_fpbz/templates/snippets/volunteers_newsletter.xml
    	addons/bestja_ucw_permissions/models.py
    	addons/bestja_volunteer/models.py
    	addons/bestja_volunteer/views/volunteer.xml

--- addons/bestja_frontend_fpbz/__openerp__.py
+++ addons/bestja_frontend_fpbz/__openerp__.py
@@ -1,6 +1,7 @@
 {
     # Theme information
     'name': "FPBŻ Theme",
+    'summary': "Html5 Responsive Bootstrap Theme for Odoo CMS",
     'description': """
     This is a custom theme made for Federacja Polskich Banków Żywności
     """,
@@ -12,11 +13,20 @@
     # assets
     'data': [
         'views/assets.xml',
-
+        
     # pages
         'templates/pages/title.xml',
         'templates/pages/home_page.xml',
         'templates/pages/footer.xml',
+        
+    # snippets
+        'templates/snippets/main_top_white_text.xml',
+        'templates/snippets/why_act_with_us.xml',
+        'templates/snippets/map_of_poland.xml',
+        'templates/snippets/quote_div.xml',
+        'templates/snippets/volunteers_newsletter.xml',
+        'templates/snippets/meet_our_volunteers.xml',
+        'templates/snippets/get_knowledge.xml'
     ],
     'application': True,
     # About information
