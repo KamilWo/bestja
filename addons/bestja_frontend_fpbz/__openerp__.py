bdcfa93fa84a9c28041917021ac827a954abe3d5
KamilWo <kamil.m.wozniak+bitbucket@gmail.com>
2015-01-25 21:31:29 +0100

    New sites - offers and regulations, new snippets

--- addons/bestja_frontend_fpbz/__openerp__.py
+++ addons/bestja_frontend_fpbz/__openerp__.py
@@ -18,15 +18,21 @@
         'templates/pages/home_page.xml',
         'templates/pages/footer.xml',
         'templates/pages/become_partner.xml',
+        'templates/pages/offers_list.xml',
+        'templates/pages/regulations.xml',
         
     # snippets
         'templates/snippets/common_snippets.xml',
         'templates/snippets/main_top_white_text.xml',
-        'templates/snippets/why_is_it_worth.xml',
+        'templates/snippets/big_title.xml',
+        'templates/snippets/why_act_with_us.xml',
         'templates/snippets/map_of_poland.xml',
         'templates/snippets/quote_div.xml',
         'templates/snippets/volunteers_newsletter.xml',
         'templates/snippets/meet_our_volunteers.xml',
+        'templates/snippets/get_knowledge.xml',
+        'templates/snippets/offer_left.xml',
+        'templates/snippets/offer_right.xml',
     ],
     'application': True,
     # Your information
